4e1a962cc95297fbf90768780e0dac8129c65a87
Shubhangi <shubhangis20@navgurukul.org>
2021-06-28 12:41:07 +0530

    changes added about connection

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // listen for requests
-app.listen(process.env.PORT || 7000, () => {
+app.listen(7000, () => {
   console.log("Server is listening on port 7000");
 });
