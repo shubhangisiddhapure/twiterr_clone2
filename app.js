12ed87c08b0a269e4b34c29904ba015deac43056
Shubhangi <shubhangis20@navgurukul.org>
2021-06-28 11:38:19 +0530

    file added

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // listen for requests
-app.listen(7000, () => {
+app.listen(process.env.PORT || 7000, () => {
   console.log("Server is listening on port 7000");
 });
